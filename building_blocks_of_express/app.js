ab510f5926dbd8bf2a55f720652ccf958cb10709
David Lormor <david.lormor@gmail.com>
2014-12-16 22:43:03 -0600

    Move blocks to separate route file.

--- building_blocks_of_express/app.js
+++ building_blocks_of_express/app.js
@@ -1,54 +1,13 @@
 var express = require('express');
 var app = express();
 
-var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({extended: false});
-
 var logger = require('./logger');
 app.use(logger);
 
 app.use(express.static(__dirname + '/public'));
 
-var blocks = {
-  'Fixed': 'Fastened securely in position',
-  'Movable': 'Capable of being moved',
-  'Rotating': 'Moving in a circle around its center'
-};
-
-app.param('name', function (req, res, next) {
-  var name = req.params.name;
-  var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
-
-  req.blockName = block;
-
-  next();
-});
-
-app.route('/blocks')
-  .get(function (req, res) {
-    res.json(Object.keys(blocks));
-  })
-  .post(parseUrlencoded, function (req, res) {
-    var newBlock = req.body;
-    blocks[newBlock.name] = newBlock.description;
-
-    res.status(201).json(newBlock.name);
-  });
-
-app.route('/blocks/:name')
-  .get(function (req, res) {
-    var description = blocks[req.blockName];
-
-    if(!description) {
-      res.status(404).json('No description found for ' + req.params.name);
-    } else {
-      res.json(description);
-    }
-  })
-  .delete(function (req, res) {
-    delete blocks[req.blockName];
-    res.sendStatus(200);
-  });
+var blocks = require('./routes/blocks');
+app.use('/blocks', blocks);
 
 app.listen(1234, function() {
   console.log("Listening on port 1234");
